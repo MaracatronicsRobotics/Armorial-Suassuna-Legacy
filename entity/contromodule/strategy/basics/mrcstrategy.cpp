f5bbc94ca64985ef04bbaa54609a6286068a8935
zsmn <zsmn@cin.ufpe.br>
2020-02-04 20:57:27 -0300

    barreira criada

--- entity/contromodule/strategy/basics/mrcstrategy.cpp
+++ entity/contromodule/strategy/basics/mrcstrategy.cpp
@@ -21,7 +21,6 @@ void MRCStrategy::configure() {
     setStrategyState(THEIRKICKOFF, new SSLStrategy_Halt());
     setStrategyState(THEIRDIRECTKICK, new SSLStrategy_Halt());
     setStrategyState(THEIRINDIRECTKICK, new SSLStrategy_Halt());
-    setStrategyState(TIMEOUT, new SSLStrategy_Attack());
-    //setStrategyState(TIMEOUT, new SSLStrategy_TimeOut());
+    setStrategyState(TIMEOUT, new SSLStrategy_TimeOut());
     setStrategyState(UNDEFINED, new SSLStrategy_Halt());
 }
