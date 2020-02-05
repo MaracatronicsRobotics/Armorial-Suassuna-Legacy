9f26bd123c313ef7f4c9a31ee604d2e3c26122b2
zsmn <zsmn@cin.ufpe.br>
2020-02-05 10:40:35 -0300

    mark player inserido

--- entity/contromodule/playbook/basics/playbook_donothing.cpp
+++ entity/contromodule/playbook/basics/playbook_donothing.cpp
@@ -14,11 +14,6 @@ int Playbook_DoNothing::maxNumPlayer() {
 void Playbook_DoNothing::configure(int numPlayers) {
     _bh_doNothing.clear();
 
-    _bh_pgk = new Behaviour_Penalty_GK();
-    _bh_atk = new Behaviour_Attacker();
-    usesBehaviour(_bh_pgk);
-    usesBehaviour(_bh_atk);
-
     for(int i=0; i<numPlayers; i++) {
         Behaviour_DoNothing *bh_doNothing = new Behaviour_DoNothing();
         usesBehaviour(bh_doNothing);
@@ -27,13 +22,6 @@ void Playbook_DoNothing::configure(int numPlayers) {
 }
 
 void Playbook_DoNothing::run(int numPlayers) {
-    QList<quint8> allPlayers = dist()->getAllPlayers();
-    setPlayerBehaviour(allPlayers.at(0), _bh_pgk);
-    setPlayerBehaviour(allPlayers.at(1), _bh_atk);
-
-    //setPlayerBehaviour(allPlayers.at(2), _bh_mkp);
-    /*
     for(int i=0; i<numPlayers; i++)
         setPlayerBehaviour(dist()->getPlayer(), _bh_doNothing.at(i));
-        */
 }
