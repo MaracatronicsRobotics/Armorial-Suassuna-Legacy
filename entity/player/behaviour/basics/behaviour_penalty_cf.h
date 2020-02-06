19c6faa84c282f2ae1da6885e95de712b04847f9
Vinícius Alves <viniciusvap1999@gmail.com>
2020-02-06 17:33:40 -0300

    Inclusão do cobrador de pênalti. Falta testar

---
+++ entity/player/behaviour/basics/behaviour_penalty_cf.h
@@ -0,0 +1,28 @@
+#ifndef BEHAVIOUR_PENALTY_CF_H
+#define BEHAVIOUR_PENALTY_CF_H
+
+#include <entity/player/behaviour/behaviour.h>
+#include <entity/player/skills/skills_include.h>
+
+class Behaviour_Penalty_CF : public Behaviour {
+private:
+    void configure();
+    void run();
+    Skill_GoToLookTo *_skill_goToLookTo;
+    Skill_Kick *_skill_kick;
+    int _state;
+
+    // Auxiliary
+    bool isGKRight();
+
+    // Skill transitions
+    enum{
+        STATE_GOTO,
+        STATE_KICK
+    };
+
+public:
+    Behaviour_Penalty_CF();
+    QString name();
+};
+#endif // BEHAVIOUR_PENALTY_CF_H
