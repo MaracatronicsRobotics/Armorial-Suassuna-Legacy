fbce512a91a925ef36c0a69332a3f3746892bbfa
zsmn <zsmn@cin.ufpe.br>
2020-02-10 20:55:23 -0300

    debug do goleiro retirado

--- entity/player/behaviour/basics/behaviour_goalkeeper.cpp
+++ entity/player/behaviour/basics/behaviour_goalkeeper.cpp
@@ -67,7 +67,6 @@ void Behaviour_GoalKeeper::run() {
             enableTransition(STATE_KICK); // se tiver perto e na nossa area, chuta!!!!
         }else if(loc()->isInsideOurArea(loc()->ball(), 1.1 * _takeoutFactor) == false){ // evitar oscilação (ruido) do visao
             enableTransition(STATE_GOTO); // goTo na bolota se n estiver na area
-            std::cout << desiredPosition.x() << " " << desiredPosition.y() << std::endl;
         }
     }else{
         enableTransition(STATE_GOTO); // caso n usemos takeout, fica dando só goToLookTo mesmo (tentativa de dominar bola)
@@ -176,9 +175,6 @@ Position Behaviour_GoalKeeper::calcAttackerBallImpact() {
 
     const Position posImpact(true, impact_x, impact_y, 0.0); // posicao de impacto (mudando só o y em teoria)
                                                                       // verificar dps a ideia de mover ele pra frente e reduzir angulação
-
-
-    std::cout << posImpact.x() << " " << posImpact.y() << std::endl;
     /* calculando posicao de impacto no y */
 
     return posImpact; // retorna o impacto, em caso de alguem ter posse da bola
