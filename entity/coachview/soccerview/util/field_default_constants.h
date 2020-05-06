4639a88c91ec9448224283b71c7fdf3c767cef05
Zilde Neto <zsmn@cin.ufpe.br>
2020-05-06 00:11:42 -0300

    now drawing default correctly

--- entity/coachview/soccerview/util/field_default_constants.h
+++ entity/coachview/soccerview/util/field_default_constants.h
@@ -36,33 +36,41 @@ namespace FieldConstantsLarc2020 {
     // WR
     const double kCenterRadius = 500.0;
     const double kDefenseRadius = 1000.0;
-    const double kDefenseStretch = 500.0;
+    const double kDefenseStretch = 2000.0;
 
-    const double kLineThickness = 10.0;
+    const double kLineThickness = 20.0;
     const double kXMax = (kFieldLength-2*kLineThickness)/2;
     const double kXMin = -kXMax;
     const double kYMax = (kFieldWidth-kLineThickness)/2;
     const double kYMin = -kYMax;
 
-    const std::size_t kNumFieldLines = 8;
+    const std::size_t kNumFieldLines = 18;
     const FieldLine kFieldLines[kNumFieldLines] = {
-      FieldLine("TopTouchLine", kXMin-kLineThickness/2, kYMax, kXMax+kLineThickness/2, kYMax, kLineThickness),
-      FieldLine("BottomTouchLine", kXMin-kLineThickness/2, kYMin, kXMax+kLineThickness/2, kYMin, kLineThickness),
-      FieldLine("LeftGoalLine", kXMin, kYMin, kXMin, kYMax, kLineThickness),
-      FieldLine("RightGoalLine", kXMax, kYMin, kXMax, kYMax, kLineThickness),
-      FieldLine("HalfwayLine", 0, kYMin, 0, kYMax, kLineThickness),
-      FieldLine("CenterLine", kXMin, 0, kXMax, 0, kLineThickness),
-      FieldLine("LeftPenaltyStretch", kXMin+kDefenseRadius, -kDefenseStretch/2, kXMin+kDefenseRadius, kDefenseStretch/2, kLineThickness),
-      FieldLine("RightPenaltyStretch", kXMax-kDefenseRadius, -kDefenseStretch/2, kXMax-kDefenseRadius, kDefenseStretch/2, kLineThickness),
+        FieldLine("TopTouchLine", kXMin-kLineThickness/2, kYMax, kXMax+kLineThickness/2, kYMax, kLineThickness),
+        FieldLine("BottomTouchLine", kXMin-kLineThickness/2, kYMin, kXMax+kLineThickness/2, kYMin, kLineThickness),
+        FieldLine("LeftGoalLine", kXMin, kYMin, kXMin, kYMax, kLineThickness),
+        FieldLine("RightGoalLine", kXMax, kYMin, kXMax, kYMax, kLineThickness),
+        FieldLine("HalfwayLine", 0, kYMin, 0, kYMax, kLineThickness),
+        FieldLine("CenterLine", kXMin, 0, kXMax, 0, kLineThickness),
+        FieldLine("LeftPenaltyStretch", kXMin+kDefenseRadius-kLineThickness/2, -kDefenseStretch/2, kXMin+kDefenseRadius-kLineThickness/2, kDefenseStretch/2, kLineThickness),
+        FieldLine("RightPenaltyStretch", kXMax-kDefenseRadius+kLineThickness/2, -kDefenseStretch/2, kXMax-kDefenseRadius+kLineThickness/2, kDefenseStretch/2, kLineThickness),
+        FieldLine("LeftGoalLeftLine", kXMin, kGoalWidth/2, kXMin-kGoalDepth-kLineThickness/2, kGoalWidth/2, kLineThickness),
+        FieldLine("LeftGoalRightLine", kXMin, -kGoalWidth/2, kXMin-kGoalDepth-kLineThickness/2, -kGoalWidth/2, kLineThickness),
+        FieldLine("RightGoalLeftLine", kXMax, kGoalWidth/2, kXMax+kGoalDepth+kLineThickness/2, kGoalWidth/2, kLineThickness),
+        FieldLine("RightGoalRightLine", kXMax, -kGoalWidth/2, kXMax+kGoalDepth+kLineThickness/2, -kGoalWidth/2, kLineThickness),
+
+        FieldLine("LeftGoalStretch", kXMin-kGoalDepth, -kGoalWidth/2, kXMin-kGoalDepth, kGoalWidth/2, kLineThickness),
+        FieldLine("RightGoalStretch", kXMax+kGoalDepth, -kGoalWidth/2, kXMax+kGoalDepth, kGoalWidth/2, kLineThickness),
+
+        FieldLine("LeftFieldLeftDefenseLine", kXMin, kDefenseStretch/2, kXMin+kDefenseRadius+kLineThickness/2, kDefenseStretch/2, kLineThickness),
+        FieldLine("LeftFieldRightDefenseLine", kXMin, -kDefenseStretch/2, kXMin+kDefenseRadius+kLineThickness/2, -kDefenseStretch/2, kLineThickness),
+        FieldLine("RightFieldLeftDefenseLine", kXMax, kDefenseStretch/2, kXMax-kDefenseRadius-kLineThickness/2, kDefenseStretch/2, kLineThickness),
+        FieldLine("RightFieldRightDefenseLine", kXMax, -kDefenseStretch/2, kXMax-kDefenseRadius-kLineThickness/2, -kDefenseStretch/2, kLineThickness),
     };
 
-    const std::size_t kNumFieldArcs = 5;
+    const std::size_t kNumFieldArcs = 1;
     const FieldCircularArc kFieldArcs[kNumFieldArcs] = {
-      FieldCircularArc("LeftFieldLeftPenaltyArc",   kXMin, kDefenseStretch/2,  kDefenseRadius-kLineThickness/2, 0,        M_PI/2,   kLineThickness),
-      FieldCircularArc("LeftFieldRightPenaltyArc",  kXMin, -kDefenseStretch/2, kDefenseRadius-kLineThickness/2, 1.5*M_PI, 2*M_PI,   kLineThickness),
-      FieldCircularArc("RightFieldLeftPenaltyArc",  kXMax, -kDefenseStretch/2, kDefenseRadius-kLineThickness/2, M_PI,     1.5*M_PI, kLineThickness),
-      FieldCircularArc("RightFieldRightPenaltyArc", kXMax, kDefenseStretch/2,  kDefenseRadius-kLineThickness/2, M_PI/2,   M_PI,     kLineThickness),
-      FieldCircularArc("CenterCircle",              0,     0,                  kCenterRadius-kLineThickness/2,  0,        2*M_PI,   kLineThickness)
+        FieldCircularArc("CenterCircle",              0,     0,                  kCenterRadius-kLineThickness/2,  0,        2*M_PI,   kLineThickness)
     };
 }
 
