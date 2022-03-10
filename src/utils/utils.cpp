6baf0528df3302d3c8df5835051638d573d3238e
Breno <bor@cin.ufpe.br>
2022-03-10 20:26:22 -0300

    Utils' Distance and Point Methods

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -23,28 +23,7 @@
 
 Utils::Utils() {
 
-    RobotIdentifier baseID;
-    Color *baseColor = new Color();
-    baseColor->set_isblue(false);
-
-    baseID.set_robotid(0);
-    baseID.set_allocated_robotcolor(baseColor);
-
-    Velocity baseVel = getVelocity(0.0f, 0.0f, 0.0f, true);
-
-    AngularSpeed baseAngularSpeed = getAngularSpeed(0.0f, false, true);
-
-    _baseControlPacket.set_allocated_robotidentifier(&baseID);
-    _baseControlPacket.set_allocated_robotvelocity(&baseVel);
-    _baseControlPacket.set_allocated_robotangularspeed(&baseAngularSpeed);
-    _baseControlPacket.set_allocated_kickspeed(&baseVel);
-    _baseControlPacket.set_dribbling(false);
-    _baseControlPacket.set_w1(0.0f);
-    _baseControlPacket.set_w2(0.0f);
-    _baseControlPacket.set_w3(0.0f);
-    _baseControlPacket.set_w4(0.0f);
 }
-
 
 Robot Utils::getRobot(int robotID, bool isBlue) {
     //Returns a blank robot object with ID: robotID; and Color: isBlue
@@ -100,21 +79,28 @@ Field Utils::getField() {
 ControlPacket Utils::getVoidControlPacket() {
     //This method returns a void controlPacket
 
-    RobotIdentifier baseID = _baseControlPacket.robotidentifier();
-    Velocity baseVel = _baseControlPacket.robotvelocity();
-    AngularSpeed baseAngularSpd = _baseControlPacket.robotangularspeed();
-    Velocity baseKickSpd = _baseControlPacket.kickspeed();
+    RobotIdentifier baseID;
+    Color *baseColor = new Color();
+    baseColor->set_isblue(false);
+
+    baseID.set_robotid(0);
+    baseID.set_allocated_robotcolor(baseColor);
+
+    Velocity baseVel = getVelocity(0.0f, 0.0f, 0.0f, true);
+    Velocity baseKickSpeed = getVelocity(0.0f, 0.0f, 0.0f, true);
+
+    AngularSpeed baseAngularSpeed = getAngularSpeed(0.0f, false, true);
 
     ControlPacket cp;
     cp.set_allocated_robotidentifier(&baseID);
     cp.set_allocated_robotvelocity(&baseVel);
-    cp.set_allocated_robotangularspeed(&baseAngularSpd);
-    cp.set_allocated_kickspeed(&baseKickSpd);
-    cp.set_dribbling(_baseControlPacket.dribbling());
-    cp.set_w1(_baseControlPacket.w1());
-    cp.set_w2(_baseControlPacket.w2());
-    cp.set_w3(_baseControlPacket.w3());
-    cp.set_w4(_baseControlPacket.w4());
+    cp.set_allocated_robotangularspeed(&baseAngularSpeed);
+    cp.set_allocated_kickspeed(&baseKickSpeed);
+    cp.set_dribbling(false);
+    cp.set_w1(0.0f);
+    cp.set_w2(0.0f);
+    cp.set_w3(0.0f);
+    cp.set_w4(0.0f);
 
     return cp;
 }
@@ -139,6 +125,8 @@ Position Utils::getPosition(float x, float y, float z, bool isInvalid) {
     pos.set_z(z);
     pos.set_isinvalid(isInvalid);
 
+    //Here we should check if the position is inside the field
+
     return pos;
 }
 
@@ -148,6 +136,8 @@ Angle Utils::getAngle(float value, bool isInDegrees, bool isInvalid) {
     angle.set_value(value);
     angle.set_isindegrees(isInDegrees);
     angle.set_isinvalid(isInvalid);
+
+    //Here we should check if the angle actually exists
 
     return angle;
 }
@@ -160,6 +150,8 @@ Velocity Utils::getVelocity(float vx, float vy, float vz, bool isInvalid) {
     velocity.set_vz(vz);
     velocity.set_isinvalid(isInvalid);
 
+    //Here we should check if is realistic (not too high i.e.)
+
     return velocity;
 }
 
@@ -171,6 +163,8 @@ Acceleration Utils::getAcceleration(float ax, float ay, float az, bool isInvalid
     acceleration.set_az(az);
     acceleration.set_isinvalid(isInvalid);
 
+    //Here we should check if is realistic (not too high i.e.)
+
     return acceleration;
 }
 
@@ -180,6 +174,8 @@ AngularSpeed Utils::getAngularSpeed(float vw, bool isInDegrees, bool isInvalid) 
     angularSpeed.set_vw(vw);
     angularSpeed.set_isindegrees(isInDegrees);
     angularSpeed.set_isinvalid(isInvalid);
+
+    //Here we should check if is realistic (not too high i.e.)
 
     return angularSpeed;
 }
@@ -217,6 +213,8 @@ FieldLineSegment Utils::getLine(Position p1, Position p2) {
     Position pos1 = getPosition(p1.x(), p1.y(), p1.z(), p1.isinvalid());
     Position pos2 = getPosition(p2.x(), p2.y(), p2.z(), p2.isinvalid());
 
+    //Here we should check if pos1 or pos2 are invalid
+
     line.set_allocated_p1(&pos1);
     line.set_allocated_p2(&pos2);
     line.set_thickness(0.0f);
@@ -228,10 +226,100 @@ FieldCircularArc Utils::getArc(Position center, float a1, float a2){
     FieldCircularArc arc;
     Position c = getPosition(center.x(), center.y(), center.z(), center.isinvalid());
 
+    //Here we should check if center is invalid
+
     arc.set_allocated_center(&c);
     arc.set_a1(a1);
     arc.set_a2(a2);
     arc.set_thickness(0.0f);
 
     return arc;
+}
+
+Position Utils::threePoints(const Position &near, const Position &far, float distance, float beta, bool isInDegrees) {
+    Angle alpha = getAngle(atan2(far.y() - near.y(), far.x() - near.x()), isInDegrees, false);
+    Angle gama = getAngle(alpha.value() + beta, isInDegrees, false);
+    Position p = getPosition(near.x() + distance*cos(gama.value()), near.y() + distance*sin(gama.value()), 0.0f, false);
+
+    if (p.isinvalid()) {
+        //Here we should verify if p is actually valid. I though something like:
+        //p = projectPointAtLine();
+    }
+
+    return p;
+}
+
+float Utils::distance(const Position &a, const Position &b) {
+    return sqrt(pow(a.x()-b.x(),2) + pow(a.y()-b.y(),2));
+}
+
+bool Utils::isPointAtLine(const Position &s1, const Position &s2, const Position &point) {
+    const Position projectedPoint = Utils::projectPointAtLine(s1, s2, point);
+    const float dist = Utils::distance(point, projectedPoint);
+    return (dist <= 0.001f);
+}
+
+bool Utils::isPointAtSegment(const Position &s1, const Position &s2, const Position &point) {
+    const bool isAtLine = Utils::isPointAtLine(s1, s2, point);
+    if (!isAtLine) {
+        return false;
+    } else {
+        Position min, max;
+        if (s2.x() >= s1.x()) {
+            min = getPosition(s1.x(), min.y(), 0.0f, false);
+            max = getPosition(s2.x(), max.y(), 0.0f, false);
+        } else {
+            min = getPosition(s2.x(), min.y(), 0.0f, false);
+            max = getPosition(s1.x(), max.y(), 0.0f, false);
+        }
+        if (s2.y() >= s1.y()) {
+            min = getPosition(min.x(), s1.y(), 0.0f, false);
+            max = getPosition(max.x(), s2.y(), 0.0f, false);
+        } else {
+            min = getPosition(min.x(), s2.y(), 0.0f, false);
+            max = getPosition(max.x(), s1.y(), 0.0f, false);
+        }
+
+        return (point.x() >= min.x() && point.x() <= max.x() && point.y() >= min.y() && point.y() <= max.y());
+    }
+}
+
+Position Utils::projectPointAtLine(const Position &s1, const Position &s2, const Position &point) {
+    const Position a = getPosition(point.x() - s1.x(), point.y() - s1.y(), point.z() - s1.z(), false);
+    const Position b = getPosition(s2.x() - s1.x(), s2.y() - s1.y(), s2.z() - s1.z(), false);
+    const float bModule = sqrt(pow(b.x(), 2) + pow(b.y(), 2));
+    const Position bUnitary = getPosition(b.x() / bModule, b.y() / bModule, b.z(), false);
+    const float scalar = Utils::scalarProduct(a, bUnitary);
+
+    return getPosition(s1.x() + (scalar * bUnitary.x()), s1.y() + (scalar * bUnitary.y()), 0.0f, false);
+}
+
+Position Utils::projectPointAtSegment(const Position &s1, const Position &s2, const Position &point) {
+    const Position projectedPoint = Utils::projectPointAtLine(s1, s2, point);
+    if(Utils::isPointAtSegment(s1, s2, projectedPoint)) {
+        return projectedPoint;
+    }
+    else {
+        const float d1 = Utils::distance(projectedPoint, s1);
+        const float d2 = Utils::distance(projectedPoint, s2);
+        return (d1<=d2)? s1 : s2;
+    }
+}
+
+float Utils::distanceToLine(const Position &s1, const Position &s2, const Position &point) {
+    const Position projectedPoint = Utils::projectPointAtLine(s1, s2, point);
+    const float distance = Utils::distance(point, projectedPoint);
+    return (distance <= 0.001f) ? 0.0f : distance;
+}
+
+float Utils::distanceToSegment(const Position &s1, const Position &s2, const Position &point) {
+    const Position projectedPoint = Utils::projectPointAtLine(s1, s2, point);
+    if(Utils::isPointAtSegment(s1, s2, projectedPoint)) {
+        return distanceToLine(s1, s2, point);
+    }
+    else {
+        const float d1 = Utils::distance(point, s1);
+        const float d2 = Utils::distance(point, s2);
+        return (d1 <= d2)? d1 : d2;
+    }
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -57,9 +57,39 @@ public:
     static FieldCircularArc getArc(Position center, float a1, float a2);
 
     //algebric methods
+    static Position threePoints(const Position &near, const Position &far, float distance, float beta, bool isInDegrees);
 
-private:
-    static ControlPacket _baseControlPacket;
+    static float distance(const Position &a, const Position &b);
+
+    static bool isPointAtLine(const Position &s1, const Position &s2, const Position &point);
+    static bool isPointAtSegment(const Position &s1, const Position &s2, const Position &point);
+    static Position projectPointAtLine(const Position &s1, const Position &s2, const Position &point);
+    static Position projectPointAtSegment(const Position &s1, const Position &s2, const Position &point);
+    static float distanceToLine(const Position &s1, const Position &s2, const Position &point);
+    static float distanceToSegment(const Position &s1, const Position &s2, const Position &point);
+
+    static float scalarProduct(const Position &A, const Position &B);
+    static float crossProduct(const Position &A, const Position &B);
+    static float getPerpendicularCoefficient(const Position &s1, const Position &s2);
+    static Position hasInterceptionSegments(const Position &s1, const Position &s2, const Position &s3, const Position &s4);
+
+    static float getAngle(const Position &a, const Position &b);
+    static float angleDiff(const float A, const Angle &B);
+    static float angleDiff(const Angle &A, const float B);
+    static float angleDiff(const Angle &A, const Angle &B);
+    static float angleDiff(const float A, const float B);
+    static void angleLimitZeroTwoPi(float *angle);
+
+    // REMINDER: vectorSum = v1 + m*v2
+    static Position vectorSum(const Position &v1, const Position &v2, float m = 1);
+    static Position vectorSum(const Position &v1, const Velocity &v2, float m = 1);
+    static Position vectorSum(const Velocity &v1, const Position &v2, float m = 1);
+    static Velocity vectorSum(const Velocity &v1, const Velocity &v2, float m = 1);
+
+    static bool checkInterval(double value, double minValue, double maxValue);
+    static void limitValue(float *value, float minValue, float maxValue);
+    static void limitMinValue(float *value, float minValue);
+    static bool approximateToZero(float *value, float error = 1e-3);
 };
 
 #endif // UTILS_H
