d630eeae50259b9c107dfbbb8003e5fa81de2657
Breno <bor@cin.ufpe.br>
2022-06-04 18:38:03 -0300

    Roles header

---
+++ src/entities/player/role/roles.h
@@ -0,0 +1,7 @@
+#ifndef ROLES_H
+#define ROLES_H
+
+#include <src/entities/player/role/role_default/role_default.h>
+#include <src/entities/player/role/role_goalkeeper/role_goalkeeper.h>
+
+#endif // ROLES_H
