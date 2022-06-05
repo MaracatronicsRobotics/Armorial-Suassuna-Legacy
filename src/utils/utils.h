5c8480d29d4ae0e3d087164e655ce42c06845a99
André Luiz <alps2@cin.ufpe.br>
2022-06-05 03:10:44 -0300

    Get Adversaries in radius function

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -109,6 +109,8 @@ public:
     static bool isInsideField(const Position &pos, float factor = 1.0);
     static bool isInsideField(const Position &pos, float dx, float dy);
 
+    QList<Robot> getAdversariesInRadius(const Position &center, float radius);
+
 private:
     static Constants* _constants;
     static WorldMap* _worldMap;
